ca631999db71b4f7daae514d6c43c51caf2d5e08
mihmonah <mih_monah@mail.ru>
2017-02-26 00:16:53 +0300

    Reworked

--- autorize.py
+++ autorize.py
@@ -5,4 +5,4 @@ def auth_data(server_ip):
     Function auth_data(ip) return pair login and password
     in format 'login/password'
     """
-    ...
+    pass
--- conprint.py
+++ conprint.py
@@ -7,16 +7,11 @@ def find_log_print_info(host, mask, log, pas, id):
     Function from this module connects to server using paramiko
     SSHclient, opens log file and prints required
     """
-    # create paramiko SSH client
     client = paramiko.SSHClient()
-    # add server key to known hosts
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    # connect to server by ip, login and password
     client.connect(host, username=log, password=pas)
-    # open an SFTP session on the SSH server
     sftp_client = client.open_sftp()
     try:
-        # open masked file in read mode
         remote_file = sftp_client.open(mask)
         prilines.find_and_print_lines(remote_file, id)
     except:
